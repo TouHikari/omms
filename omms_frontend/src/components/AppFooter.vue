ed1d5acfaffb4683a867e64884eba6040a72a9ab
TouHikari <tang20051026@foxmail.com>
2025-11-16 17:30:39 +0800

    feat: frontend create

---
+++ omms_frontend/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="footer-container">
+    <p>在线医疗管理系统（OMMS）</p>
+    <p></p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.footer-container {
+  padding: 24px 12px;
+  background-color: #001529;
+  text-align: center;
+  color: white;
+}
+</style>
